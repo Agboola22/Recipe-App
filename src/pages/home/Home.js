cc5b7136a41a209d923f5950d83b30b97d9af159
Agboola Oluwatofunmi <oluwatofunmiagboola1@gmail.com>
2024-07-19 20:43:28 +0100

    added animations

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import useFetch from '../../Hooks/useFetch'
 import RecipeCard from '../../components/RecipeCard'
+import './Home.css'
 
 const Home = () => {
     const { data: recipes, isPending, error } = useFetch('/data/db.json')
     // console.log(recipes)
     return (
         <div className=' mx-auto my-16 w-4/5'>
-            {isPending && <div>Loading.....</div>}
+            {isPending &&
+                <div class="loading-spinner">
+                    <div class="spinner"></div>
+                </div>}
             {error && <div>{error}</div>}
             {recipes && <RecipeCard recipes={recipes} />}
         </div>
