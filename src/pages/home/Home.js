0c4b942ab796f12fd19fa56e94ab4e9f526c4ab7
Agboola Oluwatofunmi <oluwatofunmiagboola1@gmail.com>
2024-07-19 20:19:14 +0100

    second commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,7 @@ import useFetch from '../../Hooks/useFetch'
 import RecipeCard from '../../components/RecipeCard'
 
 const Home = () => {
-    const { data: recipes, isPending, error } = useFetch('http://localhost:3000/recipes')
+    const { data: recipes, isPending, error } = useFetch('/data/db.json')
     // console.log(recipes)
     return (
         <div className=' mx-auto my-16 w-4/5'>
