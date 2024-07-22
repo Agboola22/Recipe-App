e5c591471b85899497cd7101038d7283d8578984
Agboola Oluwatofunmi <oluwatofunmiagboola1@gmail.com>
2024-07-22 21:53:17 +0100

    Adjusted Css styles

--- src/pages/recipes/Recipes.js
+++ src/pages/recipes/Recipes.js
@@ -10,7 +10,7 @@ const Recipes = () => {
     const recipe = data && data.recipes ? data.recipes.find(r => r.id === id) : null
 
     return (
-        <div className='mx-auto my-16 w-full md:w-3/5 bg-white box-border  text-center p-10 rounded-sm shadow'>
+        <div className='mx-auto my-16 w-4/5 md:w-3/5 bg-white box-border  text-center p-10 rounded-sm shadow text-lg'>
             {isPending && (
                 <div className="loading-spinner">
                     <div className="spinner"></div>
@@ -20,12 +20,10 @@ const Recipes = () => {
             {recipe && (
                 <>
                     <h1 className=' text-center text-[#333] mb-10 mx-auto text-3xl font-semibold'>{recipe.title}</h1>
-                    <p className='text-[#999] text-lg font-medium'>Takes: {recipe.cookingTime} to cook.</p>
-                    <ul className='p-0 flex justify-center mt-0'>
-                        {recipe.ingredients.map((ing, i) => <li key={ing} className='text-[#777] mr-3'>{ing}{i < recipe.ingredients.length - 1 ? ',' : '.'}</li>)}
-                    </ul>
+                    <p className='text-[#999] font-medium'>Takes: {recipe.cookingTime} to cook.</p>
+                    <p className='text-[#777] mr-3'><span className='text-[#333] font-medium'>Ingredients:</span> {recipe.ingredients.join(', ')}</p>
 
-                    <p className=' text-left text-lg mt-2'><span className=' font-medium'>Method:</span> {recipe.method}</p>
+                    <p className=' text-left mt-2'><span className=' text-[#333] font-medium'>Method:</span> {recipe.method}</p>
                 </>
             )}
         </div>
