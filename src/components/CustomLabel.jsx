a0aecdb732d3fd8fd00848c5a5a3721c805bb45a
Agboola Oluwatofunmi <oluwatofunmiagboola1@gmail.com>
2024-07-25 17:49:22 +0100

    created create recipe form

--- src/components/CustomLabel.jsx
+++ src/components/CustomLabel.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import InputError from './InputError.jsx'
+
 
 const CustomLabel = ({
     htmlFor,
@@ -7,22 +7,18 @@ const CustomLabel = ({
     inputType,
     inputValue,
     onChange,
-    onBlur,
-    isError,
-    errorMessage,
 
 }) => {
     return (
-        <div>
-            <label htmlFor={htmlFor} className="text-[#666666] inline-block"> {labelText} </label>
+        <div className='space-y-2 text-base'>
+            <label htmlFor={htmlFor} className='block text-left font-medium'> {labelText} </label>
             <input
                 type={inputType}
                 value={inputValue}
                 onChange={onChange}
-                onBlur={onBlur}
-                className=" appearance-none relative block w-full px-3 py-1 border border-[#666666] rounded-lg text-[#111111] opacity-35 focus:outline-none focus:opacity-100 focus:text-black"
+                required
+                className='appearance-none relative block w-full px-3 py-2 rounded-lg border border-[#d4d4d4] bg-white outline-none'
             />
-            {isError && <InputError> {errorMessage} </InputError>}
         </div>
     )
 }
--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import { useRef, useState } from 'react'
+import CustomLabel from '../../components/CustomLabel'
 
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [method, setmethod] = useState('')
+    const [cookingTime, setCookingTime] = useState('')
+    const [newIngredient, setNewIngredient] = useState('')
+    const [ingredients, setIngredients] = useState([])
+    const ingInput = useRef(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(title, method, cookingTime, newIngredient, ingredients)
+    }
+
+    const handleAdd = (e) => {
+        e.preventDefault()
+        const ing = newIngredient.trim()
+
+        if (ing && !ingredients.includes(ing)) {
+            setIngredients(prevIngredients => [...prevIngredients, ing])
+        }
+        setNewIngredient('')
+        ingInput.current.focus()
+    }
+
     return (
-        <div className='w-4/5 mx-auto'>
-            crete
+        <div className='md:w-[480px] w-[280px] mx-auto text-center my-16 text-[#555]'>
+            <h2 className='text-3xl font-semibold tracking-wider mb-8'>Add a New Recipe</h2>
+            <form onSubmit={handleSubmit}>
+                <div className=' space-y-4'>
+                    <CustomLabel
+                        htmlFor='title'
+                        labelText='Recipe Title:'
+                        inputType='text'
+                        inputValue={title}
+                        onChange={(e) => setTitle(e.target.value)}
+
+                    />
+                    <div className='space-y-2 text-base'>
+                        <label htmlFor='newIngredient' className='block text-left font-medium'>Recipe Ingredients: </label>
+                        <div className='grid md:grid-cols-5 grid-cols-4 gap-4 items-center'>
+                            <input
+                                type='text'
+                                value={newIngredient}
+                                onChange={(e) => setNewIngredient(e.target.value)}
+                                className='appearance-none relative block w-full px-3 py-2 rounded-lg border border-[#d4d4d4] bg-white outline-none box-border col-span-3 md:col-span-4'
+                                ref={ingInput}
+                            />
+                            <button
+                                onClick={handleAdd}
+                                className=' bg-[#58249c] w-full text-white px-3 py-2  rounded-xl cursor-pointer font-medium'
+                            >Add</button>
+                        </div>
+                        <p className=' text-left'>Current Ingredients: {ingredients.map(i => <em key={i}>{i}, </em>)}</p>
+                    </div>
+                    <div className='space-y-2 text-base'>
+                        <label htmlFor='method' className='block text-left font-medium'>Recipe Method: </label>
+                        <textarea
+                            type='text'
+                            value={method}
+                            onChange={(e) => setmethod(e.target.value)}
+                            required
+                            className='appearance-none relative block w-full px-3 py-2 rounded-lg border border-[#d4d4d4] bg-white outline-none box-border'
+                        />
+                    </div>
+
+                    <CustomLabel
+                        htmlFor='cookingTime'
+                        labelText='Cooking Time (minutes):'
+                        inputType='number'
+                        inputValue={cookingTime}
+                        onChange={(e) => setCookingTime(e.target.value)}
+
+                    />
+                    <button
+                        type='submit'
+                        className=' bg-[#58249c] text-white px-4     py-2 text-lg rounded-xl cursor-pointer mx-auto mt-2 font-semibold'
+                    >Submit</button>
+                </div>
+            </form>
         </div>
     )
 }
